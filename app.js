ab6439a6f2d928c43d975fed424ea3b185249dac
Luan Dinh <luan@dinh.tk>
2016-09-08 14:00:21 -0400

    fix 'no database found'

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ app.use(bodyParser.urlencoded({
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('twitter-clone.db');
 
+initDB();
+
 app.get('/', function (req, res) {
     res.send('Hello world!');
 });
@@ -35,20 +37,55 @@ app.post('/add-user', function (req, res) {
     if (userName.includes(" ")) {
         res.send("Username cannot contain any spaces.");
     }
-    db.serialize(function () {
-        db.get("SELECT userName FROM Users WHERE userName = ?", userName, function (err, row) {
-            if (row) {
-                res.send("Username already exists");
-            } else {
-                var stmt = db.prepare("INSERT INTO Users VALUES (?, ?, ?)");
-                stmt.run(userName, req.body.firstName, req.body.lastName);
-                stmt.finalize();
-                res.send("username has been added to db");
-            }
-        });
-    });
+
+    if (doesUserNameExist(userName)) {
+        res.send("Username already exists");
+    } else {
+        var stmt = db.prepare("INSERT INTO Users VALUES (?, ?, ?)");
+        stmt.run(userName, req.body.firstName, req.body.lastName);
+        stmt.finalize();
+        res.send("username has been added to db");
+    }
+});
+
+app.post('/send-tweet', function (req, res) {
+    var userName = req.body.userName;
 });
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
+
+
+function doesUserNameExist(userName) {
+    db.serialize(function () {
+        db.get("SELECT userName FROM Users WHERE userName = ?", userName, function (err, row) {
+            if (row) {
+                return true;
+            } else {
+                return false;
+            }
+        });
+    });
+}
+
+
+function initDB() {
+    db.serialize(function () {
+        /*
+         * Table: Users
+         *          - userName (TEXT - PRIMARY KEY)
+         *          - firstName (TEXT)
+         *          - lastName (TEXT)
+         */
+        db.run("CREATE TABLE IF NOT EXISTS Users (userName TEXT PRIMARY KEY, firstName TEXT, lastName TEXT)");
+
+        /*
+         * Table: Tweets
+         *          - userName (FOREIGN KEY REFERENCES Users.userName)
+         *          - tweet (TEXT)
+         *          - date (Date)
+         */
+        db.run("CREATE TABLE IF NOT EXISTS Tweets (userName REFERENCES Users(userName), tweet TEXT, date DATETIME)");
+    });
+} 
